00dddb04a2fd41adb0e2aeab1036137ab686cdde
Rachel Katz <rachel.katz@flatironschool.com>
2024-06-26 10:57:20 -0400

    commit tree ee4bb4dbff76ce65dd91da39698b1aa9aaacb589

--- index.js
+++ index.js
@@ -12,5 +12,45 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  //i want to isolate each string from the array
+  let returnVal = tutorials.map((curTutorial) => {
+    //for the current string i want to uppercase every word
+    const wordArray = curTutorial.split(' ')
+     //iterate over each word in array
+    let capitalizedArray = wordArray.map((curWord) => {
+      //use charAt to uppercase the first letter
+      return curWord = curWord[0].toUpperCase() + curWord.slice(1)
+    })
+    //put all the words back together again as a string
+    let capitalizedTutorial = capitalizedArray.join(' ')
+    return capitalizedTutorial
+
+  })
+  //return a new array with all the new strings
+  return returnVal
 }
+
+
+
+// const robots = [
+//   { name: "Johnny 5", modes: 5, isActivated: false },
+//   { name: "C3PO", modes: 3, isActivated: false },
+//   { name: "Sonny", modes: 2.5, isActivated: false },
+//   { name: "Baymax", modes: 1.5, isActivated: false },
+// ];
+
+
+// const activatedRobots = robots.map((robot) => {
+//   // return Object.assign({}, robot, {
+//   //   modes: robot.modes * 2,
+//   //   isActivated: true,
+//   // });
+//   return {
+//     ...robot,
+//     modes: robot.modes * 2,
+//     isActivated: true 
+//   }
+// });
+
+
+
